8d455d7819d55b74d8c110e370743b756921945e
Daniel E Gonzalez <dgonxalex80@gmail.com>
2026-02-03 10:56:37 -0500

    ajustes de bases

--- colores.R
+++ colores.R
@@ -11,3 +11,12 @@ c6= "#2C5697" # AZUL INSTITUCIONAL
 c7= "#F7F7F7" # FONDO GRIS CLARO WEB
 
 c8 = "#936A4d" # cafe oscuro
+
+# Paleta base (imagen)
+c11 <- "#3B5B8A"; c12 <- "#899DB9"; c13 <- "#B1BDD0"
+c21 <- "#5D7A9C"; c22 <- "#9EAFC4"; c23 <- "#BECAD7"
+c31 <- "#F28B30"; c32 <- "#F7B983"; c33 <- "#FAD1AC"
+c41 <- "#F2B33D"; c42 <- "#F7D18B"; c43 <- "#FAE1B1"
+c51 <- "#F0D9BF"; c52 <- "#F6E8D9"; c53 <- "#F9F0E5"
+
+paleta4 = c(c11,c21,c31,c41)
---
+++ init.R
@@ -0,0 +1,42 @@
+# colores base, claros y fuertes
+
+c00= "#DA4453"
+c01= "#ED5655"
+c05= "#F48C94"
+c06= "#B02C3E"   # rojo más fuerte
+
+c10= "#F9573F"
+c11= "#FC6E54"
+c15= "#FDA493"
+c16= "#D63E26"   # naranja más fuerte
+
+c20= "#F6BB42"
+c21= "#FFCE54"
+c25= "#FFE393"
+c26= "#D99A1A"   # amarillo mostaza fuerte
+
+c30= "#8CC152"
+c31= "#A0D468"
+c32= "#00B845"
+c35= "#C1E3A5"
+c36= "#6E9E3C"   # verde más fuerte
+
+c40= "#37BC9B"
+c41= "#48CFAD"
+c45= "#8BE0C9"
+c46= "#2A9D7A"   # verde-agua más fuerte
+
+c50= "#3BAFDA"
+c51= "#4FC1E9"
+c55= "#92D9F2"
+c56= "#238AB3"   # azul celeste más fuerte
+
+c60= "#4A89DC"
+c61= "#5D9CEC"
+c65= "#9CBDF4"
+c66= "#2F63B6"   # azul intenso más fuerte
+
+c7= "#2C5697"  # AZUL INSTITUCIONAL
+c8= "#F7F7F7"  # FONDO GRIS CLARO WEB
+
+
