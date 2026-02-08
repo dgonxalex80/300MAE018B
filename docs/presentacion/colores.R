4d7ae88a0afe7129cc2c4e125dce9d42acdd77d1
Daniel E Gonzalez <dgonxalex80@gmail.com>
2026-02-08 08:46:35 -0500

    ajustes guia 1,2 forma entrega

---
+++ docs/presentacion/colores.R
@@ -0,0 +1,22 @@
+# colores.R
+
+c0= "#D2BCA1" # CAQUI
+c1= "#192338" # OXFORD BLUE
+c2= "#1E2E4F" # SPACE CADET
+c3= "#31487A" # YLMN BLUE
+c4= "#8FB3E2" # JORDY BLUE
+c5= "#D9E1F1" # LEVENDER (WEB)
+
+c6= "#2C5697" # AZUL INSTITUCIONAL
+c7= "#F7F7F7" # FONDO GRIS CLARO WEB
+
+c8 = "#936A4d" # cafe oscuro
+
+# Paleta base (imagen)
+c11 <- "#3B5B8A"; c12 <- "#899DB9"; c13 <- "#B1BDD0"
+c21 <- "#5D7A9C"; c22 <- "#9EAFC4"; c23 <- "#BECAD7"
+c31 <- "#F28B30"; c32 <- "#F7B983"; c33 <- "#FAD1AC"
+c41 <- "#F2B33D"; c42 <- "#F7D18B"; c43 <- "#FAE1B1"
+c51 <- "#F0D9BF"; c52 <- "#F6E8D9"; c53 <- "#F9F0E5"
+
+paleta4 = c(c11,c21,c31,c41)
